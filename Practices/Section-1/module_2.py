dea66d7d190f9ce432b21d53638fc716af3336d4
Alex <aexace666@gmail.com>
2022-06-10 12:14:29 +1000

    Create section 1 practice questions

---
+++ Practices/Section-1/module_2.py
@@ -0,0 +1,11 @@
+# Section 1 Module 2
+# [Hello with an attitude]
+#############################################################################
+# Task Description:
+#   Write a program that inputs a name and replies with an attitude.
+#############################################################################
+# Your Output should be like this:
+# What is your name? Grok
+# So you call yourself 'Grok' huh?
+#############################################################################
+
