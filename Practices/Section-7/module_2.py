16c679cf6b5a782223fad75ca27457424d9fd656
Alex <aexace666@gmail.com>
2022-06-12 19:40:30 +1000

    created section 7 practices and copied to solutions folder

---
+++ Practices/Section-7/module_2.py
@@ -0,0 +1,18 @@
+# Section 7 Module 2
+# [Guessing Game]
+#############################################################################
+# Task Description:
+#   Create a program that asks for input on what the robot's favourite food
+#   is. Continue looping through it until the user enters the correct answer.
+#   An Example will be shown below.
+#############################################################################
+# Your Output should be like this:
+# What is my favourite food?
+# Guess? chips
+# Not even close.
+# Guess? broccoli
+# Not even close.
+# Guess? electricity
+# You guessed it! Buzzzz
+#############################################################################
+
---
+++ Practices/Section-7/module_3_part_2.py
@@ -0,0 +1,22 @@
+# Section 7 Module 3 Part 2
+# [Going up the lift]
+#############################################################################
+# Task Description:
+#   Create an elevator program that allows the user to enter a current floor
+#   and the destination floor and then print out the levels to get to the
+#   destination level. Example shown below
+#############################################################################
+# Your Output should be like this:
+# Current floor: 3
+# Destination floor: 6
+# Level 3
+# Level 4
+# Level 5
+# Level 6
+
+# Current floor: 1
+# Destination floor: 2
+# Level 1
+# Level 2
+#############################################################################
+
