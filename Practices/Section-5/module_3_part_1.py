a2876032311ecfbc6d0b6c3c4923e8a8f9ba773b
Alex <aexace666@gmail.com>
2022-06-12 17:25:27 +1000

    Created section 3 practice and added it to solutions

---
+++ Practices/Section-5/module_3_part_1.py
@@ -0,0 +1,14 @@
+# Section 5 Module 3 Part 1
+# [Secret Agent]
+#############################################################################
+# Task Description:
+#   Write a code that takes the third character of a string and put them
+#   together to piece out a secret message. Example shown below.
+#############################################################################
+# Your Output should be like this:
+# Message? cxohawalkldflghemwnsegfaeap
+# c h a l l e n g e
+
+# Message? pbaynatnahproarnsm
+# p y t h o n
+#############################################################################
