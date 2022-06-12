7716a2c044cb84241b54035268d7035108c782dc
Alex <aexace666@gmail.com>
2022-06-12 18:50:17 +1000

    created section 6 practice and added it to solutions folder

---
+++ Practices/Section-6/module_3_part_1.py
@@ -0,0 +1,18 @@
+# Section 6 Module 3 Part 1
+# [Class Roll]
+#############################################################################
+# Task Description:
+#   A class role will be entered with a space separating each name. starting,
+#   then you want to print out a sorted role with the title of Class Roll.
+#   Example is show below
+#############################################################################
+# Your Output should be like this:
+# Students: Peng Ivan Alan Jodi Macy
+# Class Roll
+# Alan
+# Ivan
+# Jodi
+# Macy
+# Peng
+#############################################################################
+
